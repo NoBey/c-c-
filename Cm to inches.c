dd316524a1ca7606e023ea8a2aebcf47112f1bfe
NoBey <786964300@qq.com>
2016-09-01 00:41:46 +0800

    cm to inches ok

--- Cm to inches.c
+++ Cm to inches.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int int main(int argc, char const *argv[]) {
+ int main(int argc, char const *argv[]) {
   /* code */
+  int cm;
+  double feet,inches=0,x;
+  scanf("%d", &cm);
+  x = cm/30.48;
+  inches = modf(x, &feet);
+  printf("%d", (int)feet);
+  if (inches!=0) {
+    printf(" %d\n", (int)(inches*12));
+  }
   return 0;
 }
