e38e42b0547de7fde0ff7847c69c087b0af5137e
NoBey <786964300@qq.com>
2016-09-01 18:54:39 +0800

    test

--- test.c
+++ test.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
-  int i=39;
+  int i;
   int ii;
-  char var[]="";
+  int o=0;
+  char var[]="12345";
   char *ip = &var;
-    // ip=ip+1;
-  strcat(var ,"sss");
-  printf("%s\n", var);
+  ii = strlen(var);
+  char ll[];
+  while (ii--) {
+     ll = *(ip+o);
+    printf("%d\n", atoi(ll) ) ;
+    i =  atoi('2');
+    printf("%d\n", i ) ;
+    o++;
+  }
+  // printf("%s\n", var);
   // printf("%c\n", *(ip+3));
   // printf("%x\n", &var);　
   // printf("%x\n", ip);　
