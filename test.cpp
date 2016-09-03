18a3167e1fe17d4cb789e474a88d430990c7594a
NoBey <786964300@qq.com>
2016-09-03 21:46:58 +0800

    c++

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main( )
+{
+  char name[50];
+   char dd[50];
+
+   cout << "请输入您的名称： ";
+   cin >> name;
+   cin >> dd;
+   cout << "您的名称是： " << dd << name << endl;
+
+}
