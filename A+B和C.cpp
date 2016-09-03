f1d00a29157fa2048609e506741add6e9bebba06
NoBey <786964300@qq.com>
2016-09-03 22:14:16 +0800

    abc

--- A+B和C.cpp
+++ A+B和C.cpp
@@ -1,15 +1,24 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-char* max(a,b){return a>b?"true":"false";}
+
+char* max1(int a, int b){
+  char s1[]="true";
+  char s2[]="false";
+
+  return a>b? s1 : s2 ;
+};
 int T;
 int A[100],B[100],C[100];
 int main() {
-  scanf("%d\n", &T);
+  cin >> T;//scanf("%d\n", &T);
+
   for (int i = 0; i < T; i++) {
-    scanf("%d %d %d", &A[i], &B[i], &C[i] );
+    cin >> A[i] >> B[i] >> C[i]; //scanf("%d %d %d", &A[i], &B[i], &C[i] );
   }
   for (int i = 0; i < T;  i++) {
-    printf("Case #%d: %s\n", (i+1), max((A[i]+B[i]), C[i]));
+    cout << "Case #" << i+1 << ": " << max1((A[i]+B[i]), C[i]) << endl;
+    //printf("Case #%d: %s\n", (i+1), max((A[i]+B[i]), C[i]));
   }
   return 0;
 }
