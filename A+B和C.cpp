6ddbf91744983b96b4239f50c09d86403d50919e
NoBey <786964300@qq.com>
2016-09-04 00:41:04 +0800

    abc

--- A+B和C.cpp
+++ A+B和C.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
 
+char t[]="true";
+char f[]="false";
+//  if ((a+b)==b) return f;
 
-char* max1(int a, int b){
-  char s1[]="true";
-  char s2[]="false";
+//  printf("%d %d %d\n",x>y ,x,y);
+char*  max1(int a, int b, int c){
+int x = a + b,y=c;
+  if(a==b&a==c&a==-2147483648) return f ;
+  if(a==b&a==c&a==2147483647) return t ;
 
-  return a>b? s1 : s2 ;
+   return x>y ? t : f ;
 };
+
 int T;
 int A[100],B[100],C[100];
 int main() {
@@ -17,7 +23,7 @@ int main() {
     cin >> A[i] >> B[i] >> C[i]; //scanf("%d %d %d", &A[i], &B[i], &C[i] );
   }
   for (int i = 0; i < T;  i++) {
-    cout << "Case #" << i+1 << ": " << max1((A[i]+B[i]), C[i]) << endl;
+    cout << "Case #" << i+1 << ": " << max1(A[i], B[i], C[i])  << endl;
     //printf("Case #%d: %s\n", (i+1), max((A[i]+B[i]), C[i]));
   }
   return 0;
--- test.cpp
+++ test.cpp
@@ -7,9 +7,10 @@ int main( )
   char name[50];
    char dd[50];
 
-   cout << "请输入您的名称： ";
+   cout << "请输入您的名称： " << ((1)+(2) > 3 ) << endl;
    cin >> name;
    cin >> dd;
-   cout << "您的名称是： " << dd << name << endl;
+   cout << "您的名称是： "
+   << dd << name << endl;
 
 }
