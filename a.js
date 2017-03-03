071606aa5405fee5c482517b6f3bb498fbb1e8b1
NoBey <786964300@qq.com>
2017-03-03 15:15:42 +0800

    a

--- a.js
+++ a.js
@@ -1,5 +1,31 @@
- process.stdout.write(23232 + '\n');
- process.stdin.on('data', function(chunk) {
-    process.stdin.pause();
-    console.log(process.argv)
- });
+var arr = [
+  [2, 3, 5, 0],
+  [5, 1, 2, -4],
+  [3, 1, -8, 1]
+];
+var k = 10;
+var val = 0;
+var l = arr[0].length;
+var h = arr.length;
+var arr2 = [];
+var coor = [];
+for (var i = 0; i < arr.length; i++) {
+  for (var ii = 0; ii < arr[i].length; ii++) {
+    arr2.push(arr[i][ii]);
+  }
+}
+
+for (var i = 0; i < arr2.length; i++) {
+  for (var tmp1 = i + 1; tmp1 < arr2.length; tmp1++) {
+    var sum = arr2[i] + arr2[tmp1];
+    if (sum > val && sum <= k) {
+      val = sum;
+      coor = [i, tmp1];
+    }
+  }
+}
+
+function c(x) {
+  return '(' + Math.ceil((x + 1) / h) + ',' + (1 + (x % l)) + ')';
+}
+console.log(c(coor[0]) + arr2[coor[0]] + ' + ' + c(coor[1]) + arr2[coor[1]] + ' = ' + val)
